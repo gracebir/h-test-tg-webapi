c22b58c768e2d1949690edbec95c901acaa0eedc
gracebir <grace.devolop@gmail.com>
2024-06-13 20:20:00 +0200

    unwrapper

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import "./index.css";
 import { WebAppProvider } from "@vkruglikov/react-telegram-web-app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <WebAppProvider options={{ smoothButtonsTransition: true }}>
-            <App />
-        </WebAppProvider>
-    </React.StrictMode>
+    <WebAppProvider options={{ smoothButtonsTransition: true }}>
+        <App />
+    </WebAppProvider>
 );
