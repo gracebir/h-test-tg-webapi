c0a3d613946e2c83f6f2f5044d46aeeb7430bb7c
gracebir <grace.devolop@gmail.com>
2024-06-13 21:13:56 +0200

    change the verion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 /** @format */
 import { MainButton, useShowPopup } from "@vkruglikov/react-telegram-web-app";
 function App() {
-    const showPopup = useShowPopup();
-
-    const handleClick = () =>
-        showPopup({
-            message: "Hello world",
-        });
-
-    return <MainButton text='SHOW POPUP' onClick={handleClick} />;
+    const showPopupOnClick = async () => {
+        const showPopup = useShowPopup();
+        const message =
+            "Thanks for using react-mini-app! I hope it helps you to create awesome Telegram Mini apps!";
+        await showPopup({ title: "Hello world!", message: message });
+    };
+    return (
+        <div>
+            <h1>Hello </h1>
+            <button
+                onClick={showPopupOnClick}
+                className='bg-gray-500 hover:bg-gray-400'
+            >
+                show message
+            </button>
+        </div>
+    );
 }
 
 export default App;
