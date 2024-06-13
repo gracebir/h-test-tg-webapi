89723644ae5224419534e41855ee5860aa06a8c1
gracebir <grace.devolop@gmail.com>
2024-06-13 20:10:25 +0200

    setting the telegram

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,10 @@ function App() {
 
     const handleClick = () =>
         showPopup({
-            message: "Hello, I am popup",
+            message: "Hello world",
         });
 
-    return (
-        <main className='min-h-screen flex items-center'>
-            <div className='mx-auto'>
-                <h1 className='text-3xl lg:text-5xl font-bold'>Hello world</h1>
-                <MainButton text='SHOW POPUP' onClick={handleClick} />
-            </div>
-        </main>
-    );
+    return <MainButton text='SHOW POPUP' onClick={handleClick} />;
 }
 
 export default App;
